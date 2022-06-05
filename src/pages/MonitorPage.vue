45497fecbfb906ebb02584fe636c16f9c0108f9e
dobutamine <tim.antonius@outlook.com>
2022-06-05 22:43:47 +0200

    lijntje

--- src/pages/MonitorPage.vue
+++ src/pages/MonitorPage.vue
@@ -1,19 +1,210 @@
 <template>
   <q-page class="bg-black">
-    <div class="row justify-center items-start q-ma-es">
-      <div class="col bg-white">TIMER</div>
-      <div class="col bg-green">ALARM</div>
-      <div class="col bg-white">ALARMMESSAGE</div>
+    <div class="row justify-center q-ma-es" :style="upperRowHeight">
+      <div class="col text-yellow"></div>
+      <div class="col text-yellow">ALARM</div>
+      <div class="col text-black bg-yellow text-center">ALARMMESSAGE</div>
     </div>
-    <div class="row justify-center items-start q-ma-es">
-      <div class="col-8 bg-white">column one</div>
-      <div class="col-2 bg-green">HR</div>
-      <div class="col-2 bg-white">column three</div>
+    <div class="row justify-center q-ma-es text-green" :style="rowHeight">
+      <div class="col-8">II</div>
+      <div class="col-2">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>HF</div>
+            <div style="font-size: 10px">200</div>
+            <div style="font-size: 10px">80</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 52px">
+            135
+          </div>
+        </div>
+      </div>
+      <div class="col-2 text-purple-12">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>Pols</div>
+            <div style="font-size: 10px">200</div>
+            <div style="font-size: 10px">80</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 36px">
+            135
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-center q-ma-es text-purple-12" :style="rowHeight">
+      <div class="col-8">Pleth(1)</div>
+      <div class="col-2">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>SpO2</div>
+            <div style="font-size: 10px">100</div>
+            <div style="font-size: 10px">92</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 52px">97</div>
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>PFI</div>
+            <div style="font-size: 10px"></div>
+            <div style="font-size: 10px"></div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 36px">
+            1.2
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-center q-ma-es text-purple-13" :style="rowHeight">
+      <div class="col-8">Pleth(2)</div>
+      <div class="col-2">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>SpO2(2)</div>
+            <div style="font-size: 10px">100</div>
+            <div style="font-size: 10px">92</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 52px">95</div>
+        </div>
+      </div>
+      <div class="col-2"></div>
+    </div>
+    <div class="row justify-center q-ma-es text-pink" :style="rowHeight">
+      <div class="col-8">ABP</div>
+      <div class="col-3">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>ABP</div>
+            <div style="font-size: 10px">80</div>
+            <div style="font-size: 10px">40</div>
+          </div>
+          <div class="col-10 text-left q-pt-sm" style="font-size: 52px">
+            60/40 (42)
+          </div>
+        </div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <div class="row justify-center q-ma-es text-white" :style="rowHeight">
+      <div class="col-8">Resp</div>
+      <div class="col-2">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>RF</div>
+            <div style="font-size: 10px">80</div>
+            <div style="font-size: 10px">20</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 52px">45</div>
+        </div>
+      </div>
+      <div class="col-2"></div>
+    </div>
+    <div class="row justify-center q-ma-es text-yellow" :style="rowHeight">
+      <div class="col-8">CO2</div>
+      <div class="col-2">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>etCO2</div>
+            <div style="font-size: 10px">7.5</div>
+            <div style="font-size: 10px">3.5</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 52px">
+            4.5
+          </div>
+        </div>
+      </div>
+      <div class="col-2"></div>
+    </div>
+    <div class="row justify-center q-ma-es text-orange" :style="rowHeight">
+      <div class="col-3">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>NIBD</div>
+            <div style="font-size: 10px">80</div>
+            <div style="font-size: 10px">40</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 40px">
+            60/40 (42)
+          </div>
+        </div>
+      </div>
+      <div class="col-1"></div>
+      <div class="col-2"></div>
+      <div class="col-2 text-green">
+        <div class="row">
+          <div class="col" style="font-size: 14px">
+            <div>Thuid</div>
+            <div style="font-size: 10px">38</div>
+            <div style="font-size: 10px">36</div>
+          </div>
+          <div class="col-10 text-left q-pa-sm" style="font-size: 40px">
+            37.1
+          </div>
+        </div>
+      </div>
+      <div class="col-2"></div>
+      <div class="col-2"></div>
     </div>
 
-    <div class="row absolute-bottom justify-center items-start q-ma-es">
+    <div class="row absolute-bottom justify-center items-start q-ma-sm">
       <div class="q-gutter-xs">
-        <q-btn color="grey" label="Button" v-for="n in 12" :key="`xs-${n}`" />
+        <q-btn
+          class="text-black"
+          color="yellow"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Stil"
+        />
+        <q-btn
+          color="grey"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Pauzeer alarmen"
+        />
+        <q-btn
+          color="grey-8"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="<<"
+        />
+        <q-btn
+          color="grey"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Start/ Stop"
+        />
+        <q-btn
+          color="grey"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Nullen"
+        />
+        <q-btn
+          color="grey"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="QRS geluid"
+        />
+        <q-btn
+          color="grey"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Voorval zien"
+        /><q-btn
+          color="grey"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Monitor Standby"
+        />
+        <q-btn
+          color="grey-8"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label=">>"
+        />
+        <q-btn
+          color="grey"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Hoofd opzet"
+        />
+        <q-btn
+          color="blue-10"
+          style="height: 60px; width: 80px; font-size: 12px"
+          label="Hoofd scherm"
+        />
       </div>
     </div>
 
@@ -28,17 +219,20 @@ export default {
     return {
       websocket: null,
       getVitalsTimer: null,
+      rowHeight: "height': 150px",
+      upperRowHeight: "height': 50px",
       butConnectText: "CONNECT TO MANIKIN",
       butConnectClass: "q-ma-lg bg-red text-white",
     };
   },
   methods: {
     onResize() {
-      console.log(this.$q.screen.height);
       this.$root.$emit("resize", {
         width: this.$q.screen.width,
         height: this.$q.screen.height,
       });
+      this.rowHeight = `height: ${this.$q.screen.height / 9}px`;
+      this.upperRowHeight = `height: ${this.$q.screen.height / 9 / 3}px`;
     },
     toggleConnection() {
       if (this.websocket) {
